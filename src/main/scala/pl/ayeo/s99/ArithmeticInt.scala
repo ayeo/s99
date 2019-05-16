c3575777bb6f1576b50ea3c5027cb076461e5001
ayeo <me@ayeo.pl>
2019-05-16 20:57:57 +0200

    Break long lines

--- src/main/scala/pl/ayeo/s99/ArithmeticInt.scala
+++ src/main/scala/pl/ayeo/s99/ArithmeticInt.scala
@@ -36,7 +36,8 @@ class ArithmeticInt(private val i: Int) {
 
   /**
     * P34 (**) Calculate Euler's totient function phi(m).
-    * Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r <= m) that are coprime to m.
+    * Euler's so-called totient function phi(m) is defined as the
+    * number of positive integers r (1 <= r <= m) that are coprime to m.
     * scala> 10.totient
     * res0: Int = 4
     */
@@ -98,7 +99,10 @@ class ArithmeticInt(private val i: Int) {
 
   /**
     * P37 (**) Calculate Euler's totient function phi(m) (improved).
-    * See problem P34 for the definition of Euler's totient function. If the list of the prime factors of a number m is known in the form of problem P36 then the function phi(m>) can be efficiently calculated as follows: Let [[p1, m1], [p2, m2], [p3, m3], ...] be the list of prime factors (and their multiplicities) of a given number m. Then phi(m) can be calculated with the following formula:
+    * See problem P34 for the definition of Euler's totient function.
+    * If the list of the prime factors of a number m is known in the form of problem P36 then the function phi(m>) c
+    * an be efficiently calculated as follows: Let [[p1, m1], [p2, m2], [p3, m3], ...] be the list of prime
+    * factors (and their multiplicities) of a given number m. Then phi(m) can be calculated with the following formula:
     * phi(m) = (p1-1)*p1(m1-1) * (p2-1)*p2(m2-1) * (p3-1)*p3(m3-1) * ...
     *
     * Note that ab stands for the bth power of a.
--- src/main/scala/pl/ayeo/s99/WorkingWithLists.scala
+++ src/main/scala/pl/ayeo/s99/WorkingWithLists.scala
@@ -105,7 +105,8 @@ class WorkingWithLists {
 
   /**
     * P08 (**) Eliminate consecutive duplicates of list elements.
-    * If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+    * If a list contains repeated elements they should be replaced with a single copy of the element. The order of
+    * the elements should not be changed.
     * Example:
     *
     * scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
@@ -123,7 +124,8 @@ class WorkingWithLists {
     * Example:
     *
     * scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    * res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+    * res0: List[List[Symbol]] =
+    * List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
     */
   def pack[A](list: List[A], sublist: List[A] = List()): List[List[A]] = {
     if (list.isEmpty) List() :+ sublist
@@ -135,7 +137,9 @@ class WorkingWithLists {
 
   /**
     * P10 (*) Run-length encoding of a list.
-    * Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E.
+    * Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+    * Consecutive duplicates of elements are encoded as tuples (N, E) where N is
+    * the number of duplicates of the element E.
     * Example:
     *
     * scala> encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
@@ -154,7 +158,8 @@ class WorkingWithLists {
 
   /**
     * P11 (*) Modified run-length encoding.
-    * Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N, E) terms.
+    * Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into
+    * the result list. Only elements with duplicates are transferred as (N, E) terms.
     * Example:
     *
     * scala> encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
@@ -200,7 +205,8 @@ class WorkingWithLists {
 
   /**
     * P13 (**) Run-length encoding of a list (direct solution).
-    * Implement the so-called run-length encoding data compression method directly. I.e. don't use other methods you've written (like P09's pack); do all the work directly.
+    * Implement the so-called run-length encoding data compression method directly. I.e. don't use other methods
+    * you've written (like P09's pack); do all the work directly.
     * Example:
     *
     * scala> encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
@@ -278,7 +284,8 @@ class WorkingWithLists {
 
   /**
     * P18 (**) Extract a slice from a list.
-    * Given two indices, I and K, the slice is the list containing the elements from and including the Ith element up to but not including the Kth element of the original list. Start counting the elements with 0.
+    * Given two indices, I and K, the slice is the list containing the elements from and including the Ith element up
+    * to but not including the Kth element of the original list. Start counting the elements with 0.
     * Example:
     *
     * scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
