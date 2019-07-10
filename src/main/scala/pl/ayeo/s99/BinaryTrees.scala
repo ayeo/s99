917508012d362838600dbd9e0cc4e7fa86b7a957
ayeo <me@ayeo.pl>
2019-07-10 23:56:03 +0200

    Wrap comment lines

--- src/main/scala/pl/ayeo/s99/BinaryTrees.scala
+++ src/main/scala/pl/ayeo/s99/BinaryTrees.scala
@@ -15,8 +15,13 @@ object Node {
 object Tree {
   /**
     * P55 (**) Construct completely balanced binary trees.
-    * In a completely balanced binary tree, the following property holds for every node: The number of nodes in its left subtree and the number of nodes in its right subtree are almost equal, which means their difference is not greater than one.
-    * Define an object named Tree. Write a function Tree.cBalanced to construct completely balanced binary trees for a given number of nodes. The function should generate all solutions. The function should take as parameters the number of nodes and a single value to put in all of them.
+    * In a completely balanced binary tree, the following property holds for every node: The number of nodes in its left
+    * subtree and the number of nodes in its right subtree are almost equal, which means their difference is not greater
+    * than one.
+    *
+    * Define an object named Tree. Write a function Tree.cBalanced to construct completely balanced binary trees for a
+    * given number of nodes. The function should generate all solutions. The function should take as parameters the
+    * number of nodes and a single value to put in all of them.
     *
     * scala> Tree.cBalanced(4, "x")
     * res0: List(Node[String]) = List(T(x T(x . .) T(x . T(x . .))), T(x T(x . .) T(x T(x . .) .)), ...
